e212c39feeb3c2f12010ad9142d8b01446efb5bc
Gabriel Marciano Arante de Assis <gabriel.marciano@dinfor.uniceub>
2026-02-23 16:49:01 -0300

    desafio4.py

---
+++ desafio4.py
@@ -0,0 +1,9 @@
+a = int(input("Digite Um numero"))
+b = int(input("Digite Um numero"))
+c = int(input("Digite Um numero"))
+d = int(input("Digite Um numero"))
+e = int(input("Digite Um numero"))
+f = int(input("Digite Um numero"))
+Soma = a + b, c + d, e + f
+
+print ("Soma dos Dois numeros e: ", Soma)
