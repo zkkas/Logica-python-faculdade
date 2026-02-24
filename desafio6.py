439870354ebbb474721cd4791fa75e90da1bd01a
zkkas <gabrielpcgamer23@gmail.com>
2026-02-23 23:17:44 -0300

    desafio6.py

---
+++ desafio6.py
@@ -0,0 +1,12 @@
+A = float(input("nota 1: ")) # aqui o usario bota os dados da nota 
+B = float(input("nota 2: ")) #aqui o usario bota os dados da nota
+C = float(input("nota 3: ")) #aqui o usario bota os dados da nota
+
+soma = float(A * 2 + B * 3 + C * 5) / 10 
+media = soma 
+if ():
+   soma > 10.0 
+else:
+    soma = 10.0
+
+print(f"A Media e: {media} ") 
