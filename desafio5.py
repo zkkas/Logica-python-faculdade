44c67e1df3d23a43e12bbea15d482a538a5578a1
zkkas <gabrielpcgamer23@gmail.com>
2026-02-23 23:09:52 -0300

    desafio5.py

---
+++ desafio5.py
@@ -0,0 +1,11 @@
+A = float(input("nota 1: ")) # aqui o usario bota os dados da nota 
+B = float(input("nota 2: ")) #aqui o usario bota os dados da nota
+soma = float(A * 3.5 + B * 7.5) / 11 # aqui funciona no seguinte pego os dois valores da variavel que usario botou e depois mutiplico pelos pesos e somo no final e divido por 11
+
+media = soma # aqui eu chamo o resultado que nossa variavel soma que guardo o nosso resultado do calculo anterior
+if ():
+    media < 10 
+else:
+    media = 10.0
+
+print(f"A Media e: {media:.5f} ") # Depois eu imprimo tudo na tela  chamando a variavel media com um limitação de 5 numeros apois a virgula
