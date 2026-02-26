7467795bf354448a9c91a2dc006e086302643f61
zkkas <gabrielpcgamer23@gmail.com>
2026-02-26 19:37:21 -0300

    Desafio7.py

---
+++ desafio7.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um numero: "))
+b = int(input("Digite um numero: "))
+c = int(input("Digite um numero: "))
+d = int(input("Digite um numero: "))
+
+formula = ((a * b) - (c * d))
+
+print("A diferença e: ", formula)
