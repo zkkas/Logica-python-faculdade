9c26e44d31df3570351865626bb04d0adcc6e450
zkkas <gabrielpcgamer23@gmail.com>
2026-02-26 20:08:49 -0300

    Comentarios

--- desafio4.py
+++ desafio4.py
@@ -1,9 +1,9 @@
-a = int(input("Digite Um numero"))
-b = int(input("Digite Um numero"))
-c = int(input("Digite Um numero"))
-d = int(input("Digite Um numero"))
-e = int(input("Digite Um numero"))
-f = int(input("Digite Um numero"))
-Soma = a + b, c + d, e + f
+a = int(input("Digite Um numero")) # aqui o usario bota os dados 
+b = int(input("Digite Um numero")) # aqui o usario bota os dados 
+c = int(input("Digite Um numero")) # aqui o usario bota os dados 
+d = int(input("Digite Um numero")) # aqui o usario bota os dados 
+e = int(input("Digite Um numero")) # aqui o usario bota os dados 
+f = int(input("Digite Um numero")) # aqui o usario bota os dados 
+Soma = a + b, c + d, e + f # aqui eu faço uma variavel pra soamr os numeros das outra variaveis
 
-print ("Soma dos Dois numeros e: ", Soma)
+print ("Soma dos Dois numeros e: ", Soma) # aqui eu imprimo a soma dos numeros
