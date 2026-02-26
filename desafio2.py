8b9f5c542e4328a91d2d8e1b4be653b52551d65b
zkkas <gabrielpcgamer23@gmail.com>
2026-02-26 20:11:29 -0300

    Comentarios

--- desafio2.py
+++ desafio2.py
@@ -1,4 +1,4 @@
-a = int(input ("Digite um numero: "))
-b = int(input("Digite Um numero: "))
+a = int(input ("Digite um numero: ")) # aqui eu declaro a variavel 'a' do tipo inteiro  com entrada de dados pra o usario botar
+b = int(input("Digite Um numero: ")) # aqui eu declaro a variavel 'b' do tipo inteiro com entradad de dados pra o usario botar
 
-print(f"X = {a + b}")
+print(f"X = {a + b}") #aqui eu faço a soma do a + b que vira nosso x
--- desafio3.py
+++ desafio3.py
@@ -1,5 +1,5 @@
-raio = float(input("Digite um Numero"))
-n = 3.14159
-area = n *  (raio ** 2)
+raio = float(input("Digite um Numero")) #aqui declaro uma variavel chamada raio do tipo float com a entrada de dados
+n = 3.14159 #aqui a variavel n guarda o numero pi
+area = n *  (raio ** 2) # aqui a variavel area faz o calculo pra saber a area e guarda o resultado na variavel
 
-print(f"A = {area:.4f}")
+print(f"A = {area:.4f}") # aqui eu imprimo tudo pro usario o resultado da area
