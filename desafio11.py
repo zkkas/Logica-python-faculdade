83c3ee50981728fa27732f6002b706aeae4f6f94
zkkas <gabrielpcgamer23@gmail.com>
2026-03-08 02:57:52 -0300

    salvando arquivos antes de mudar de branch

---
+++ desafio11.py
@@ -0,0 +1,7 @@
+raio = int(input("informe um raio: ")) #entrada de dados de raio
+
+pi = 3.14159 #variavel do pi 
+
+calculo = (4/3) * pi * (raio ** 3) #calculo do volume
+
+print(f"o volume {calculo:.3f}") #printa o volume
