4edc179434be66d9f93704ca85391f01e48f728e
zkkas <gabrielpcgamer23@gmail.com>
2026-03-16 17:13:09 -0300

    resolvido

--- desafio36.py
+++ desafio36.py
@@ -1,12 +1,12 @@
 from math import sqrt 
-a,b,c = map(float, input('').split())
+a,b,c = map(float, input().split())
 
-delta = b**2 - 4*a*c
+deltas = (b**2) - 4*a*c
 
-if delta <= 0:
-     print('impossivel calcular')
-else:
-    R1 = (-b + sqrt(delta)) / (2*a)
-    R2 = (-b - sqrt(delta)) / (2*a)
+if deltas > 0 and a != 0:
+    R1 = (-b + sqrt(deltas)) / (2*a)
+    R2 = (-b - sqrt(deltas)) / (2*a)
     print('R1 = {:.5f}'.format(R1))
-    print('R2 = {:.5f}'.format(R2))    
+    print('R2 = {:.5f}'.format(R2))  
+else:
+ print('impossivel calcular')
